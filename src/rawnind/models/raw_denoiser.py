32e9f2b4cf33fcba116cdea39fe0437264749bd9
johannes hanika <hanatos@gmail.com>
2025-09-22 16:09:31 +0200

    jddcnn: put vkdt network arch

--- src/rawnind/models/raw_denoiser.py
+++ src/rawnind/models/raw_denoiser.py
@@ -50,6 +50,75 @@ class UtNet2(Denoiser):
         self,
         in_channels: int,
         funit: int = 32,
+        activation: str = "ReLU",
+        preupsample: bool = False,
+    ):
+        super().__init__(in_channels=in_channels)
+        # pm = 'reflect' # not implemented you suckers
+        pm = 'zeros'
+        self.enc0 = nn.Conv2d(INPUT_CHANNELS_COUNT, 32, 3, padding='same', padding_mode=pm)
+        self.enc1 = nn.Conv2d(32, 48, 3, padding='same', padding_mode=pm)
+        self.enc2 = nn.Conv2d(48, 64, 3, padding='same', padding_mode=pm)
+        self.enc3 = nn.Conv2d(64, 80, 3, padding='same', padding_mode=pm)
+        self.enc4 = nn.Conv2d(80, 112, 3, padding='same', padding_mode=pm)
+        self.enc5 = nn.Conv2d(112, 112, 3, padding='same', padding_mode=pm)
+
+        # self.extr = nn.Conv2d(32, 32, 3, padding='same', padding_mode=pm)
+
+        self.dec0 = nn.Conv2d(112+112, 112, 3, padding='same', padding_mode=pm)
+        self.con0 = nn.Conv2d(112, 112, 3, padding='same', padding_mode=pm)
+        self.dec1 = nn.Conv2d(112+80, 80, 3, padding='same', padding_mode=pm)
+        self.con1 = nn.Conv2d(80, 80, 3, padding='same', padding_mode=pm)
+        self.dec2 = nn.Conv2d(80+64, 64, 3, padding='same', padding_mode=pm)
+        self.con2 = nn.Conv2d(64, 64, 3, padding='same', padding_mode=pm)
+        self.dec3 = nn.Conv2d(64+48, 48, 3, padding='same', padding_mode=pm)
+        self.con3 = nn.Conv2d(48, 48, 3, padding='same', padding_mode=pm)
+        self.dec4 = nn.Conv2d(48+32, 16, 3, padding='same', padding_mode=pm)
+        self.con4 = nn.Conv2d(16, 16, 3, padding='same', padding_mode=pm)
+        self.dec5 = nn.Conv2d(16+INPUT_CHANNELS_COUNT, 12, 3, padding='same', padding_mode=pm)
+        self.con5 = nn.Conv2d(12, 12, 3, padding='same', padding_mode=pm)
+
+    def forward(self, I):
+        extr = F.relu(self.enc0(I))
+        # extr = F.relu(self.extr(extr))
+        x_128 = self.pool(extr) # self.pool(F.relu(self.extr(extr)))
+        # x_128 = self.pool(F.relu(self.enc0(I)))
+        x_64  = self.pool(F.relu(self.enc1(x_128)))
+        x_32  = self.pool(F.relu(self.enc2(x_64)))
+        x_16  = self.pool(F.relu(self.enc3(x_32)))
+        x_8   = self.pool(F.relu(self.enc4(x_16)))
+        x_4   = self.pool(F.relu(self.enc5(x_8)))
+        
+        x     = F.relu(self.dec0(torch.cat([self.upsample(x_4), x_8],   1)))
+        x     = F.relu(self.con0(x))
+        # x     = F.relu(self.con0a(x))
+        # x     = F.relu(self.dec1(torch.cat([self.upsample(x_8), x_16],  1)))
+        x     = F.relu(self.dec1(torch.cat([self.upsample(x),   x_16],  1)))
+        x     = F.relu(self.con1(x))
+        # x     = F.relu(self.con1a(x))
+        x     = F.relu(self.dec2(torch.cat([self.upsample(x),   x_32],  1)))
+        x     = F.relu(self.con2(x))
+        # x     = F.relu(self.con2a(x))
+        x     = F.relu(self.dec3(torch.cat([self.upsample(x),   x_64],  1)))
+        x     = F.relu(self.con3(x))
+        # x     = F.relu(self.con3a(x))
+        x     = F.relu(self.dec4(torch.cat([self.upsample(x),   x_128], 1)))
+        # x     = F.relu(self.dec5(torch.cat([self.upsample(x),   extr],  1)))
+        x     = F.relu(self.con4(x))
+        # x     = F.relu(self.con4a(x))
+        x     = F.relu(self.dec5(torch.cat([self.upsample(x),   I],  1)))
+        x     = F.relu(self.con5(x))
+        # x     = F.relu(self.con5a(x))
+        
+        return x
+
+
+
+class OriginalUtNet2(Denoiser):
+    def __init__(
+        self,
+        in_channels: int,
+        funit: int = 32,
         activation: str = "LeakyReLU",
         preupsample: bool = False,
     ):

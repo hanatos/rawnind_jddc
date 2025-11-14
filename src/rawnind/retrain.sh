273874016fd7edb69adab8800936d0f15fc846ee
johannes hanika <hanatos@gmail.com>
2025-11-14 10:36:13 +0100

    low learning rate, train longer without nan

---
+++ src/rawnind/retrain.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python train_denoiser_bayer2prgb.py --batch_size_clean 0 --disable_retry_wait --continue_training_from_last_model_if_exists
---
+++ src/rawnind/train.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python train_denoiser_bayer2prgb.py --batch_size_clean 0 --disable_retry_wait

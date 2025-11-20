fd702a13dfc460a834add39ea922991350ac0a67
johannes hanika <hanatos@gmail.com>
2025-11-20 13:41:32 +0100

    settings for small network

--- src/rawnind/copy.sh
+++ src/rawnind/copy.sh
@@ -2,5 +2,11 @@
 # copy the latest (and hopefully greatest) version of the trained model weights to the vkdt data dir:
 dir=$(ls -td  ../../models/rawnind_denoise/DenoiserTrainingBayerToProfiledRGB_4ch-* | head -1)
 dat=$(ls -t  ${dir}/saved_models/iter_*.pt.dat | head -1)
-echo installing ${dat} as jddcnn weights
-cp ${dat} ~/.config/vkdt/data/jddcnn-weights-heavy.dat
+sz=$(ls -l ${dat} | cut -d" " -f5)
+model="heavy"
+if (( 4308408 < 10000000 ))
+then
+  model="light"
+fi
+echo installing ${dat} as jddcnn weights ${model}
+cp ${dat} ~/.config/vkdt/data/jddcnn-weights-${model}.dat
